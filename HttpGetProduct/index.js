2403614b38c39bfb2ef88738b6bdeb32dfab646f
大溝桂 <kay.akubicharm.dev@gmail.com>
2022-11-29 17:00:31 +0900

    update

--- HttpGetProduct/index.js
+++ HttpGetProduct/index.js
@@ -1,12 +1,12 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    const productId = (req.query.productId || (req.body && req.body.productId));
+    const responseMessage = productId
+        ? "The product name for your product id {" + productId + "} is Starfruit Explosion"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
+        context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
